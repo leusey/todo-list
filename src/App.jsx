6262e2135c5e7a945c35c1c0cdefe93c1c565c9e
YeSeul <70742008+leusey@users.noreply.github.com>
2025-03-24 22:13:05 -0700

    week 3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
+import { useState } from "react";
 
 function App() {
+  const [newTodo, setNewTodo] = useState("My first todo");
+
   return (
     <div>
       <h1>My To-dos</h1>
       <TodoForm />
+      <p>{newTodo}</p>
       <TodoList />
     </div>
   );
